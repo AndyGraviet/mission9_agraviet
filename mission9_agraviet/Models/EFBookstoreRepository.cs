a6ae21ad741b069a233a47d690235ff7f4444b5c
AndyGraviet <93163571+AndyGraviet@users.noreply.github.com>
2023-02-23 10:34:11 -0700

    Initial

---
+++ mission9_agraviet/Models/EFBookstoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+namespace mission9_agraviet.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepository(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Book> Books => context.Books;
+    }
+}
---
+++ mission9_agraviet/Models/IBookstoreRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace mission9_agraviet.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
